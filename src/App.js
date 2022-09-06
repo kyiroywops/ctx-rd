7e845aaa201982f5661edf1b6caff717eff636d7
kyiroywops <102992278+kyiroywops@users.noreply.github.com>
2022-09-06 18:32:17 -0400

    blog cambio ctx

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Header from './componentes/Header';
+import Inicio from './componentes/Inicio';
+import Blog from './componentes/Blog';
+import Post from './componentes/Post';
+import AcercaDe from './componentes/AcercaDe';
+import Error404 from './componentes/Error404';
+import styled from 'styled-components';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+	return (
+		<BrowserRouter>
+			<ContenedorPrincipal>
+				<Header />
+				
+				<Main>
+					<Routes>
+						<Route path="*" element={<Error404 />} />
+						<Route path="/" element={<Inicio />} />
+						<Route path="/blog" element={<Blog />} />
+						<Route path="/post/:id" element={<Post />} />
+						<Route path="/acerca-de" element={<AcercaDe />} />
+					</Routes>
+				</Main>
+			</ContenedorPrincipal>
+		</BrowserRouter>
+	);
 }
 
+const ContenedorPrincipal = styled.div`
+	padding: 40px;
+	width: 90%;
+	max-width: 700px;
+`;
+
+const Main = styled.main`
+	background: #fff;
+	padding: 40px;
+	border-radius: 10px;
+	box-shadow: 0px 0px 5px rgba(129, 129, 129, 0.1);
+`;
+ 
 export default App;
